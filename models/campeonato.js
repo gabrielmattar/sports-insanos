500db91d7b085722262b4256965cc95e6cc44f03
Gabriel Mattar <gabrielmattar41@gmail.com>
2016-06-07 09:10:11 -0300

    modularizacao  da criacao do campeonato

--- models/campeonato.js
+++ models/campeonato.js
@@ -16,6 +16,8 @@ var campSchema =  new Schema({
   }]
 });
 
+
+
 campSchema.pre('find', function(next) {
   // do stuff
   this.populate('adm');
@@ -30,6 +32,62 @@ campSchema.pre('findOne', function(next) {
   next();
 });
 
+campSchema.methods.Cria = function(tamanho, nomec, admin, nomet, User, Time) {
+
+  var timesc = nomet;
+  var times = [];
+    for(var time of timesc){
+      Time.findOne({nometime: time}, function(err, team){
+        if(err){
+          console.log(err);
+        }
+        else {
+          User.findOne({username: admin}, function(err, admi){
+            if(err){
+              console.log(err);
+            }
+            else {
+              times.push(team);
+              if(times.length == tamanho) {
+                var keys = [];
+                keys.push({
+                  times: times
+                })
+                var i = times.length/2;
+                //Nessa sessao abaixo usamos um time em branco para preencher o resto das chaves
+                //Se a entrada foram 8 times entao vao haver 3 chaves, a primeira com 8 times a
+                //segunda com 4 a terceira com 2 e a primeira com 1
+                Time.findOne({nometime: '-'}, function(err, branco){
+                  while(i >= 1){
+                    var brancos = [];
+                    for(var j = 0; j < i; j++){
+                      brancos.push(branco._id);
+                    }
+                    keys.push({
+                      times: brancos
+                    })
+                    i=i/2;
+                  }
+                  var newCamp =  Camp({
+                    nome: nomec,
+                    numerotimes: tamanho,
+                    chaves: keys,
+                    adm:admi
+                  } );
+
+                  newCamp.save(function(err) {
+                    if (err) throw err;
+                    console.log('Campeonato created!');
+                  });
+                });
+
+              }
+            }
+          });
+        }
+      });
+    }
+}
 
 var Camp = mongoose.model('Camp', campSchema);
 module.exports = Camp;
