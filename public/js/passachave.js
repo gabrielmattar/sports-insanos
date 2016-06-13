97c97841bb4a218bf5f84488344bab338c12383e
Gabriel Mattar <gabrielmattar41@gmail.com>
2016-06-13 09:32:34 -0300

    Avanca carinhas ok

--- public/js/passachave.js
+++ public/js/passachave.js
@@ -3,13 +3,23 @@ function passachave(nometime, indice) {
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       //Encontrar o proximo maluquinho
-      document.getElementById().innerHTML = xhttp.responseText;
+      var indicenext= Math.floor(Number(indice)/2);
+      document.getElementById('-' + indicenext).innerHTML = nometime;
     }
   };
+  var indicenext= Math.floor(Number(indice)/2);
+  var prox = document.getElementById('-' + indicenext);
+  prox.innerHTML = nometime;
+  prox.setAttribute("onclick", "passachave('" + nometime + "'," + indicenext + ")");
+  prox.id = nometime+indicenext;
+
+  var chavinha = prox.parentNode.parentNode.parentNode.id;
+  chavinha -= 1;
+
   var clickado = document.getElementById(nometime+indice);
   var chave = clickado.parentNode.parentNode.parentNode;
   var campeonato = chave.parentNode;
-  var path = campeonato.id + '/' + chave.id + '/' + nometime + '/' + indice;
+  var path = campeonato.id + '/' + chavinha + '/' + nometime + '/' + indice;
   xhttp.open("GET", "/avanca/" + path, true);
   xhttp.send();
 }
